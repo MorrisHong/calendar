4d4a09714022eb16043a7faafb4ba82dbd1514b8
Administrator <Administrator@MSDN-SPECIAL>
2019-01-20 14:34:01 +0900

    Update myAnswer of virtualCalendar

--- src/govlmo/calendar/Calendar.java
+++ src/govlmo/calendar/Calendar.java
@@ -16,6 +16,12 @@ public class Calendar {
 //		System.out.println("반복횟수를 입력하세요. : ");
 //		int repeat=sc.nextInt();
 		
+//		월을 입력하면 해당월의 달력을 출력한다.
+//		달력은 모양은 1단계에서 작성한 모양으로 만든다.
+//		1일은 일요일로 정해도 무방하다.
+//		-1을 입력받기 전까지 반복 입력받는다.
+//		프롬프트를 출력한다.
+		
 		
 		int month = 0;
 		while(true) {
@@ -25,10 +31,16 @@ public class Calendar {
 			if(month == -1) {
 				break;
 			}
-			if(month > 12) {
+			if(month > 12 || month == 0) {
 				continue;
 			}
-			System.out.printf("%d월은 %d일까지 있습니다. \n\n", month, cal.getMaxDaysOfMonth(month));
+			if(cal.getMaxDaysOfMonth(month) == 31) {
+				cal.printSampleCal("29 30 31");
+			}else if(cal.getMaxDaysOfMonth(month) == 30) {
+				cal.printSampleCal("29 30");
+			}else {
+				cal.printSampleCal("");
+			}
 		}
 		
 		System.out.println("Bye~");
@@ -39,13 +51,15 @@ public class Calendar {
 		return MAX_DAYS[month - 1];
 	}
 
-	public void printSampleCal() {
+	public void printSampleCal(String add) {
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		System.out.println(add);
+		System.out.println();
 	}
 
 }
